1785735410416395a7cbc895d0361f6b567552a3
gentledot <gentledot.wp@gmail.com>
2019-11-19 23:48:05 +0900

    Event API - Response 구현 (URI)

--- src/main/java/net/gentledot/demorestapi/events/EventController.java
+++ src/main/java/net/gentledot/demorestapi/events/EventController.java
@@ -1,21 +1,27 @@
 package net.gentledot.demorestapi.events;
 
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.net.URI;
 
+/* deprecated 이므로 대체 함수를 확인해야 함 : 20191119_gentledot*/
 import static org.springframework.hateoas.server.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.ControllerLinkBuilder.methodOn;
 
 @Controller
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
 public class EventController {
 
-    @PostMapping("/api/events")
-    public ResponseEntity createEvent() {
-        URI createUri = linkTo(methodOn(EventController.class).createEvent()).slash("{id}").toUri();
-
-        return  ResponseEntity.created(createUri).build();
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event event) {
+        URI createUri = linkTo(EventController.class).slash("{id}").toUri();
+        event.setId(1004);
+        return ResponseEntity.created(createUri).body(event);
     }
 }
--- src/test/java/net/gentledot/demorestapi/events/EventControllerTest.java
+++ src/test/java/net/gentledot/demorestapi/events/EventControllerTest.java
@@ -1,5 +1,6 @@
 package net.gentledot.demorestapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,14 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+
+import java.time.LocalDateTime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -20,15 +26,35 @@ public class EventControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     /* 입력 값을 전달 시 JSON 응답으로 status=201 나오는지 확인 */
     @Test
     public void createEvent() throws Exception {
+        // 요청 입력
+        Event event = Event.eventBuilder()
+                .name("Spring")
+                .description("REST API Dev. with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2019, 11, 19, 14, 21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019, 11, 20, 14, 21))
+                .beginEventDateTime(LocalDateTime.of(2019, 11, 21, 14, 21))
+                .endEventDateTime(LocalDateTime.of(2019, 11, 26, 14, 21))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 5번 출구")
+                .build();
+
+        // 요청 동작
         mockMvc.perform(post("/api/events/")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaTypes.HAL_JSON)
-//                .content()
-        )   // 요청
+                .content(objectMapper.writeValueAsString(event))
+        )
                 .andDo(print())
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                // org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+                .andExpect(jsonPath("id").exists());
     }
 }
