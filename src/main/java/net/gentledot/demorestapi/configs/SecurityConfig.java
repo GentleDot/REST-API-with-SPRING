ce95017bc6fd9af02db9c5e9ad0020000b72a3d5
gentledot <gentledot.wp@gmail.com>
2019-12-05 13:42:32 +0900

    Event API - configuring authentication server(inMemory)

--- src/main/java/net/gentledot/demorestapi/configs/SecurityConfig.java
+++ src/main/java/net/gentledot/demorestapi/configs/SecurityConfig.java
@@ -5,10 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -52,16 +50,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
         // security chain 을 타고 해당 request 인가
 //        http.authorizeRequests()
 //                .mvcMatchers("/docs/index.html").anonymous()
 //                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
 
-        http.formLogin()
-                .and()
-                .authorizeRequests()
-                    .mvcMatchers(HttpMethod.GET, "/api/**").authenticated();
-    }
+//        http.formLogin()
+//                .and()
+//                .authorizeRequests()
+//                    .mvcMatchers(HttpMethod.GET, "/api/**").authenticated();
+//    }
 }
